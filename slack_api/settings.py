f24b75b85ad07c5b4ffe254540b35de49bb667fd
bukeme <basseyukemme@gmail.com>
2022-10-30 20:45:11 +0100

    edited allowed host settings

--- slack_api/settings.py
+++ slack_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--_nd%*_c3yevpn=eb(8*o*migs##ldl#*=$ywhm25@=a*pt-d1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uk-slack-api.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
